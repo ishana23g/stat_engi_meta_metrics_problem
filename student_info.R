951653727653fb3a26d30ce19f570a91b569a65a
Ishana Desktop PC <ishana23g@vt.edu>
2025-08-20 12:44:29 -0400

    Saved data and graphs and some interpretations of it

--- student_info.R
+++ student_info.R
@@ -4,7 +4,9 @@ library(readr)
 library(tidyr)
 
 # assuming some data is in a file saved in "student_responses.csv"
-file_path <- "student_responses.csv"
+# include the path to the file
+curr_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+file_path <- file.path(curr_path, "student_responses.csv")
 
 # Actually checking if it exists.
 # In this case it doesn't exist, so we can just generate some random data to
@@ -32,7 +34,7 @@ if (file.exists(file_path)) {
         responses <- c(rep(1, correct), rep(0, num_students - correct))
         sample(responses)  # Shuffle the responses
       }),
-      .name_repair = ~ paste0("item_", 1:num_tests)
+      .name_repair = ~ paste0("test_", 1:num_tests)
     ))
   
   # Save the newly generated data for future use
@@ -40,9 +42,6 @@ if (file.exists(file_path)) {
   cat("New random dataset saved as 'student_responses.csv'.\n")
 }
 
-# make sure that colums with item_1 to item_10 are binary factors
-student_data <- student_data %>%
-  mutate(across(starts_with("item_"), ~ factor(.x, levels = c(0, 1))))
 
 # do a quick look at the data
 print(head(student_data))
@@ -54,14 +53,80 @@ str(student_data)
 summary(student_data)
 
 # Some example analyses that can be done
+# Main question trying to answer is:
+# How would you conduct an initial evaluation of item or test performance?
 # --------------------------------------
-
-# some more of the basic exploratory data analysis
-# do a basic pair wise correlation between items in a
-# plot with ggplot2
 
 
 # calculate a total score for each student
+# NOTE that I am keeping the data as numeric for the 0s and 1s
+# so that I can add them easily together
+student_scores <- student_data %>%
+  rowwise() %>%
+  mutate(total_score = sum(c_across(starts_with("test_")))) %>%
+  ungroup()
+
+# view the first few rows of the scores
+print(head(student_scores))
+
 # rank the top students
+top_students <- student_scores %>%
+  arrange(desc(total_score)) %>%
+  slice_head(n = 10)
+print(top_students)
+
+# rank the bottom students
+bottom_students <- student_scores %>%
+  arrange(total_score) %>%
+  slice_head(n = 10)
+print(bottom_students)
+
+
+# looking at these individual students also trying to group students together that are all having 
+# similar scores could be interesting with a corr matrix between students, i.e. just doing a 
+# inner product between one student's set of scores and another's scores.
+
 # visualize the distribution of scores
-# 
+ggplot(student_scores, aes(x = total_score)) +
+  geom_histogram(binwidth = 1, fill = "#00BFC4", color = "black", alpha = 0.7) +
+  labs(title = "Distribution of Total Scores", x = "Total Score", y = "Number of Students") +
+  theme_minimal()
+
+# from this we can see get a read of the majority of students and where most of them 
+# fall score wise.
+
+# Another graph to look at is a box plot with jittered points that can help showcase where the
+# majority, minority, and if there is some kind of weird spread in the data.
+
+ggplot(student_scores, aes(x = "", y = total_score)) +
+  geom_boxplot(outlier.shape = NA, fill = "#00BFC4", alpha = 0.7) +
+  geom_jitter(width = 0.1, color = "black", alpha = 0.5) +
+  labs(title = "Box Plot of Total Scores", x = "", y = "Total Score") +
+  theme_minimal()
+
+# in this case I would say this plot does not help actually give much information about the data.
+# However, we can tell that getting a really low score such as a 3 or even a full 10 
+# is pretty rare. 
+# Moreover, from these plots we actually see we have no student that got an all 0 score.
+
+# For looking at the each of the test performances
+# However here when we are looking at the distribution of 0s and 1s
+# with a bar chart is a better idea since they are actually looking at categorical factors.
+
+# making sure that the tests are actually binary factors
+student_data <- student_data %>%
+  mutate(across(starts_with("test_"), ~ factor(.x, levels = c(0, 1))))
+
+# first we should count the data for the tests. 
+test_data = student_data %>%
+  pivot_longer(cols = starts_with("test_"), names_to = "test", values_to = "response") %>%
+  group_by(test, response) %>%
+  summarise(count = n(), .groups = 'drop')
+
+ggplot(test_data, aes(x = test, y = count, fill = response)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  labs(title = "Distribution of Test Responses", x = "Test", y = "Count") +
+  scale_fill_manual(values = c("0" = "#F8766D", "1" = "#00BFC4"), labels = c("Incorrect", "Correct")) +
+  theme_minimal()
+
+   
